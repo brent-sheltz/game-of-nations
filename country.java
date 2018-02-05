fb17d29476fdf722df970f62e576174eee531c8c
brent-sheltz <35032261+brent-sheltz@users.noreply.github.com>
2018-02-05 10:29:36 -0500

    Add files via upload

---
+++ country.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class country here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class country
+{
+    // instance variables - replace the example below with your own
+    
+    /**
+     * Constructor for objects of class country
+     */
+    public country(String name , int population, int agriculture, int patriotism , int income , Boolean is_arsernal_nation)
+    {
+
+       
+    }
+
+   
+}
---
+++ game.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/**
+ * Write a description of class game here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class game
+{
+    country usa = new country("usa",10,10,10,10,true) ;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
