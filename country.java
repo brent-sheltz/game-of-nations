2620a668a6ae3cb3d5647e8331b0c0d888c87503
brent-sheltz <35032261+brent-sheltz@users.noreply.github.com>
2018-02-08 10:31:40 -0500

    Add files via upload

--- country.java
+++ country.java
@@ -14,74 +14,71 @@ public class country
     private int industry; 
     private int income;
     private int arsenal_pt;
-    
+
     private int army;
     private int navy;
-    
-    
+
     /**
      * Constructor for objects of class country
      */
     public country(String name , int population, int agriculture, int patriotism ,
     int industry ,int income , Boolean is_arsernal_nation, int army , int navy)
     {
-           this.name = name;
-           this.population = population;
-           this.agriculture=agriculture;
-           this.patriotism = patriotism;
-           this.industry = industry;
-           this.income = income;
-           this.army = army;
-           this.navy = navy;
-           if(is_arsernal_nation == true)
-           {
-               this.arsenal_pt = 5 ; 
-    
-            }
+        this.name = name;
+        this.population = population;
+        this.agriculture=agriculture;
+        this.patriotism = patriotism;
+        this.industry = industry;
+        this.income = income;
+        this.army = army;
+        this.navy = navy;
+        if(is_arsernal_nation == true)
+        {
+            this.arsenal_pt = 5 ; 
+
+        }
 
     }
-    
-    
+
+    public  String tostring()
+    {
+
+        return ""+ army;
+    }
+    public void add(String country, String resource, int ammount )
+    {
+        resource =  resource;
+        if ( resource.equals("army"))
+        {
+            this.army = army + ammount;
+
+        }
+        if ( resource.equals("navy"))
+        {
+            this.navy = navy + ammount;
+
+        }
+
+    }
     public void build_army()
     {
         if (population >= 1)
         {
-            
-            
-            
-            
+
         }
-        
-        
-        
     }
     public void loss(String obj , int subtract)
     {
-            if ( obj.equals("army"))
-            {
+        if ( obj.equals("army"))
+        {
             this.army = army - subtract;
-            
-            }
-           
-        
-    
-    
-    
-    
-    
+
+        }
+
     
     
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     
     
 }
